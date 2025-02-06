1183b7ad0267bd2353f53839ac581b96f527687f
Junior <68521496+jrftw@users.noreply.github.com>
2025-02-06 16:59:53 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
+// MARK: MAIN IMPORTS
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
+// MARK: MAIN RENDER
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
